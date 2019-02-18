62adf0e0a88426c47ec4c586e67db85563d4e75b
kudzurunner <sdio@tuta.io>
2019-02-18 12:59:38 +0300

    fixed builds

--- build.py
+++ build.py
@@ -3,6 +3,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="kudzurunner")
-    builder.add(settings={"arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.version": 15, "compiler.runtime": "MD"},
-                options={}, env_vars={}, build_requires={})
+    builder.add_common_builds()
     builder.run()
